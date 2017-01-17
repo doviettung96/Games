925e1f7a9e95b69b88043d18e6cb199714b19e37
tungdv <tungdv@ows.edu.vn>
2017-01-17 14:27:05 +0700

    finish beta version

--- fit_bar.js
+++ fit_bar.js
@@ -3,16 +3,55 @@
  */
 $(document).ready(function () {
     var bar = $(".bar");
+    var meter = $(".meter");
+    var box2 = $("#box2");
     var num_click = 0;
     var txt = "";
-    var button = $("button");
     var def_height = Math.random() * 1000;
+    var speed;
+    var button = $("button");
+
+    txt += "<br> Goal: " + def_height + "<br>";
+
+    button.mousedown(function () {
+        num_click = 0;
+        def_height = Math.random() * 1000;
+        speed = 0;
+        bar.animate(
+            {
+                height: '100px'
+            }
+        );
+        meter.animate({
+            marginLeft: 0
+        });
+
+    });
+    button.mouseup(function () {
+        txt = "";
+        $("#result").html(txt);
+    });
+
+    box2.mousedown(function (event) {
+        // var left = event.pageX
+        speed = event.pageX;
+        meter.animate({
+            marginLeft: event.pageX - 50
+        });
+
+    });
+
+    box2.mouseup(function () {
+        box2.stop();
+        meter.stop();
+    });
+
     bar.mousedown(function () {
         bar.animate(
             {
-                height: '+=200px'
+                height: '+=500px'
 
-            }, 8000);
+            }, speed);
 
     });
 
@@ -22,30 +61,17 @@ $(document).ready(function () {
         txt = "Height of the bar:  " + bar.height() + "<br>";
         txt += "<br> Goal: " + def_height + "<br>";
 
-        if (bar.height() > def_height && num_click <= 3) {
-            txt += "<br>" + "<b>YOU WIN</b> <br>" + "<b>START NEW GAME</b> <br>";
+        var height_difference = bar.height() - def_height;
+        var square_difference = height_difference * height_difference;
+        if (square_difference < 2500 && num_click <= 2) {
+            txt += "<br>" + "<b>YOU WIN</b> <br>";
         }
-        else if(bar.height() < def_height && num_click > 3){
-            txt += "<br>" + "<b>YOU LOSE</b> <br>" + "<b>START NEW GAME</b> <br>";
-
+        else if (height_difference > 50 || num_click > 2) {
+            txt += "<br>" + "<b>YOU LOSE</b> <br>";
         }
 
         $("#result").html(txt);
     });
 
-    button.mousedown(function () {
-        num_click = 0;
-        def_height = Math.random() * 1000;
-        bar.animate(
-            {
-                height: '100px'
-            }
-        );
-
-    });
-    button.mouseup(function () {
-        txt = "";
-        $("#result").html(txt);
-    });
 
 });
